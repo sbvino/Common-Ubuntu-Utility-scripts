401675ea5be6513687406c41d78d30e253d16108
Vinod K Selvaraj <35976152+sbvino@users.noreply.github.com>
2021-06-25 13:41:37 +0530

    Create Install_Utilities.sh

---
+++ Install_Utilities.sh
@@ -0,0 +1,16 @@
+# Prereqs and docker
+sudo apt-get update &&
+    sudo apt-get install -yqq \
+        curl \
+        git \
+        apt-transport-https \
+        ca-certificates \
+        gnupg-agent \
+        software-properties-common \
+        net-tools \
+        vim \
+        nano \
+        dnsutils \
+        jq \
+        firewalld \
+        cron
